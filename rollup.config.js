3e9be4156b07e66e1b7767dfd92fb759a18ea10d
YeongMin Ahn <ahn0min95@gmail.com>
2023-05-29 21:50:24 +0900

    Chore: 번들링을 위한 rollup 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
 export default {
-  input: ["src/iframe.js", "src/open.js"], // Replace 'main.js' with the entry point of your application
+  input: ["lib/index.js"], // Replace 'main.js' with the entry point of your application
   output: {
-    dir: "dist", // Replace 'bundle.js' with the desired name of the output file
-    format: "es",
+    file: "bundle.js", // Replace 'bundle.js' with the desired name of the output file
+    format: "iife",
+    globals: {
+      "state/index.js": "index_js",
+    },
   },
 };
